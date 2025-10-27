e565b47c02e77dc8413458939900b79c4856d6cf
손현욱 <hus0316@ariatech.kr>
2025-10-27 10:15:53 +0900

    init

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
